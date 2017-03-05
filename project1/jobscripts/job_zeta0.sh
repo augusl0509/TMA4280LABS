8397d6e9f7b0e815c006fd774b84c278f6da5107
August <augustlb@stud.ntnu.no>
2017-03-05 11:44:06 +0100

    Fix for one of the job scripts

--- project1/jobscripts/job_zeta0.sh
+++ project1/jobscripts/job_zeta0.sh
@@ -6,6 +6,6 @@
 #PBS -l select=1:ncpus=32:mpiprocs=16
 
 cd $PBS_O_WORKDIR
-./TMA4280LABS/project1/zeta0/machutest
+./TMA4280LABS/project1/zeta0/zetautest
 ./TMA4280LABS/project1/zeta0/estimatepi 1000000
-./TMA4280LABS/project1/zeta0/machvtest
+./TMA4280LABS/project1/zeta0/zetavtest
