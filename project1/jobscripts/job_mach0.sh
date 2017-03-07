0445b9dd9d296711d3cb44c8ad72ec3b5ba08795
August <augustlb@stud.ntnu.no>
2017-03-07 17:38:08 +0100

    Serial job scripts fix

--- project1/jobscripts/job_mach0.sh
+++ project1/jobscripts/job_mach0.sh
@@ -3,9 +3,7 @@
 #PBS -N estimatepi_mach0
 #PBS -A imf_tma4280
 #PBS -l walltime=00:01:00
-#PBS -l select=1:ncpus=32:mpiprocs=16
+#PBS -l select=1:ncpus=20
 
 cd $PBS_O_WORKDIR
-./TMA4280LABS/project1/mach0/machutest
-./TMA4280LABS/project1/mach0/estimatepi 1000000
 ./TMA4280LABS/project1/mach0/machvtest
