9d56ac069f8dec7cf4a02f7f23c742475bc9a348
August <augustlb@stud.ntnu.no>
2017-04-19 20:32:07 +0200

    Added job scripts for scaling tests

---
+++ project2/jobscripts/scalingtest/job_hybrid_poisson_p2_t2_n8192.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#PBS -N mpi_poisson_p2_t2_n8192
+#PBS -A imf_tma4280
+#PBS -l walltime=00:10:00
+#PBS -l select=1:ncpus=20:mpiprocs=2:ompthreads=2
+
+cd $PBS_O_WORKDIR
+module load gcc openmpi
+mpiexec ./TMA4280LABS/project2/p_poisson/hybrid_poisson/build/poisson 8192
